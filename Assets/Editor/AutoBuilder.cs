f92b4cffb2ce97300de7d104e43fd0eb150659bc
paperfrog <paperfrog@naver.com>
2020-11-23 09:40:57 +0900

    Add auto build script
    항상 자동 빌드 스크립트는 Asset/Editor 밑에 있어야하며 클래스 이름은 상관없다.

---
+++ Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class AutoBuilder
+{
+
+    [MenuItem("Build/Build AOS(Debug")]
+    public static void Build_AOS_Debug()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = FindEnabledEditorScenes();
+        buildPlayerOptions.locationPathName = string.Format("Build(AOS)/Test_{0}.apk", PlayerSettings.bundleVersion);
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.Development;
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+
+
+    [MenuItem("Build/Build IOS")]
+    public static void Build_IOS()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = FindEnabledEditorScenes();
+        buildPlayerOptions.locationPathName = "Build(IOS)";
+        buildPlayerOptions.target = BuildTarget.iOS;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+
+        return EditorScenes.ToArray();
+    }
+}
