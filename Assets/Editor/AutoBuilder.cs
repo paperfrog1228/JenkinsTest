286e1af0ab0294e6637e76527526e972ce09a38c
paperfrog <paperfrog@naver.com>
2020-11-23 10:30:03 +0900

    Modify auto build script
    스크립트 문제인 듯

--- Assets/Editor/AutoBuilder.cs
+++ Assets/Editor/AutoBuilder.cs
@@ -30,33 +30,6 @@ public class AutoBuilder
         }
     }
 
-
-
-    [MenuItem("Build/Build IOS")]
-    public static void Build_IOS()
-    {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = FindEnabledEditorScenes();
-        buildPlayerOptions.locationPathName = "Build(IOS)";
-        buildPlayerOptions.target = BuildTarget.iOS;
-        buildPlayerOptions.options = BuildOptions.None;
-
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        BuildSummary summary = report.summary;
-
-        if (summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-        }
-
-        if (summary.result == BuildResult.Failed)
-        {
-            Debug.Log("Build failed");
-        }
-    }
-
-
-
     private static string[] FindEnabledEditorScenes()
     {
         List<string> EditorScenes = new List<string>();
